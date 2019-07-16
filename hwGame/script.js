782c0dc2828fce69c7df1e2576c9a7a3a6a6ac42
Victoria <viktoriag373@gmail.com>
2019-07-16 16:22:17 +0300

    меняется цвет дива в зависимости от того, что ввели. если не угадали число. то красный, если угадали - зеленый. номер попытки и значение хода добавляется в одном блоке, каждый раз с новой строки, добавляя дочерний элемент

--- hwGame/script.js
+++ hwGame/script.js
@@ -1,40 +1,42 @@
 window.onload = function() {
-    var inputAnswer = document.querySelector('.answer');
-    var button = document.querySelector('.button');
-    var textStatus = document.querySelector('.status');
-    var log = document.querySelector('.log');
-    var input = document.querySelector('.input')
-    var count = 0;
+    var inputAnswer = document.querySelector('.answer'); 
+    var button = document.querySelector('.button');     
+    var textStatus = document.querySelector('.status');  
+    var log = document.querySelector('.log');           
+    var count = 0;      
 
-    var logData = 'Попытка №: ';
-    var inputNumber = '';
-
-    function random(min, max) {
-        return Math.round(min + Math.random() * (max - min));
+    function random(min, max) {                         
+        return Math.round(min + Math.random() * (max - min)); 
     }
-    var randomNumber = random(1, 5);
-    console.log(randomNumber);
+    var randomNumber = random(1, 5);                     
+    console.log(randomNumber);                          
     
 
     //addEventListener(type, func)
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function() {        
         var inputValue = inputAnswer.value;
+        inputAnswer.value = '';
         count++;
         if (inputValue == randomNumber) {
             textStatus.innerText = ' Поздравляем! Вы угадали число!';
-        } else if(inputValue == 0 && isNaN(inputValue)) {
+            textStatus.style.backgroundColor = '#024b08';
+        } else if(inputValue == 0 || isNaN(inputValue)) {
             textStatus.innerText = 'Вы ввели не то, что нужно';
+            textStatus.style.backgroundColor = '#4b022c';
         } else {
             if ( inputValue < randomNumber ) {
                 textStatus.innerText = 'Ваше число меньше';
             } else if(inputValue > randomNumber) {
                 textStatus.innerText = 'Ваше число больше';
             }
+            textStatus.style.backgroundColor = '#4b022c';
         }
-        logData += count + ', ';
-        log.innerText = logData;
 
-        inputNumber += inputValue + ', ';
-        input.innerText = inputNumber;
+        var logItem = document.createElement('p');
+        logItem.innerText = 'Попытка №: ' + count + ', Ваш ход - ' + inputValue;
+        log.appendChild(logItem);
+
+
+
     });
 };

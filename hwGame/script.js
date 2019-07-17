6fa29e73f0e51b031fba73c00f0b7fab1c95aa9e
Victoria <viktoriag373@gmail.com>
2019-07-17 22:09:32 +0300

    1. Переделала UI. 2. Доработала игровую логику.

--- hwGame/script.js
+++ hwGame/script.js
@@ -1,25 +1,76 @@
 window.onload = function() {
     var inputAnswer = document.querySelector('.answer'); 
-    var button = document.querySelector('.button');     
+    var buttonAnswer = document.querySelector('.button_answer');  
+    var buttonStartStop = document.querySelector('.button_start_stop');    
     var textStatus = document.querySelector('.status');  
-    var log = document.querySelector('.log');           
+    var log = document.querySelector('.log');   
+    var randomNumber;
     var count = 0;      
+
+
+
+    function setInputEnabled(enabled) {
+        //inputAnswer.style.display = enabled ? '' : 'none';
+        //buttonAnswer.style.display = enabled ? '' : 'none';
+
+        if (enabled) {
+            inputAnswer.style.display = '';
+            buttonAnswer.style.display = '';
+        } else {
+            inputAnswer.style.display = 'none';
+            buttonAnswer.style.display = 'none';
+        }
+
+
+        // inputAnswer.disabled = !enabled;
+        // buttonAnswer.disabled = !enabled;
+    }
 
     function random(min, max) {                         
         return Math.round(min + Math.random() * (max - min)); 
     }
-    var randomNumber = random(1, 5);                     
-    console.log(randomNumber);                          
-    
 
-    //addEventListener(type, func)
-    button.addEventListener('click', function() {        
+    function starNewGame() {
+        log.innerText = '';
+        count = 0;
+        randomNumber = random(1, 5);
+        console.log(randomNumber);
+        
+        textStatus.style.backgroundColor = '#198543';
+        textStatus.innerText = 'Компьютер загадал число';
+        setInputEnabled(true);
+
+        inputAnswer.onkeypress = function(event) {   //передается событие
+            if (event.code === 'Enter') {
+                checkGame();
+            }
+        }
+
+        buttonAnswer.onclick = checkGame;
+           
+        buttonStartStop.value = 'Закончить игру'
+        buttonStartStop.onclick = function() {
+            textStatus.innerText = 'Конец игры((\n Компьютер загадал число ' + randomNumber;
+            textStatus.style.backgroundColor = '#000';
+            endGame();
+        }
+    }
+
+    function endGame() {
+        setInputEnabled(false);
+        buttonStartStop.value = 'Новая игра'
+        buttonStartStop.onclick = starNewGame;
+    }
+
+    function checkGame() {
         var inputValue = inputAnswer.value;
         inputAnswer.value = '';
         count++;
         if (inputValue == randomNumber) {
             textStatus.innerText = ' Поздравляем! Вы угадали число!';
             textStatus.style.backgroundColor = '#024b08';
+            endGame();
+
         } else if(inputValue == 0 || isNaN(inputValue)) {
             textStatus.innerText = 'Вы ввели не то, что нужно';
             textStatus.style.backgroundColor = '#4b022c';
@@ -35,8 +86,11 @@ window.onload = function() {
         var logItem = document.createElement('p');
         logItem.innerText = 'Попытка №: ' + count + ', Ваш ход - ' + inputValue;
         log.appendChild(logItem);
+    }
 
 
 
-    });
+
+              
+    starNewGame();
 };
